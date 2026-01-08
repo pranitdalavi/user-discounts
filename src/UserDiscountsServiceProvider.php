ec2a70b538803c731fd5aeade7c5a3bcadec0fbb
Pranit <pranit.dalavi98@gmail.com>
2026-01-08 19:48:14 +0530

    changes

--- src/UserDiscountsServiceProvider.php
+++ src/UserDiscountsServiceProvider.php
@@ -6,16 +6,31 @@ use Illuminate\Support\ServiceProvider;
 
 class UserDiscountsServiceProvider extends ServiceProvider
 {
+    /**
+     * Register services.
+     */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/Config/discounts.php', 'discounts');
+        // Merge package config with app config
+        $this->mergeConfigFrom(__DIR__ . '/Config/discounts.php', 'discounts');
     }
 
+    /**
+     * Bootstrap services.
+     */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // Load migrations directly from package
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        // Publish config file
         $this->publishes([
-            __DIR__.'/Config/discounts.php' => config_path('discounts.php'),
+            __DIR__ . '/Config/discounts.php' => config_path('discounts.php'),
         ], 'config');
+
+        // Publish migrations to the application's database/migrations folder
+        $this->publishes([
+            __DIR__ . '/../database/migrations/' => database_path('migrations'),
+        ], 'migrations');
     }
 }
